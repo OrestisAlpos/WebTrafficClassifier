d295387a2bf473ea3067d4018901fedef78e9b24
OrestisAlpos <or.al@hotmail.com>
2017-04-23 03:03:22 +0300

    Run MLP Dataset1 mse sgd dropout 0.2/0.4

--- MLP_plot_all.py
+++ MLP_plot_all.py
@@ -25,12 +25,12 @@ def get_model(num_hid_layers, cells_per_layer, dropout_rate):
 		model.add(Dense(cells_per_layer, activation='relu'))
 		model.add(Dropout(dropout_rate))
 
-	model.add(Dense(5, activation='softmax'))
+	model.add(Dense(1, activation='sigmoid'))#softmax se multiclass, sigmoid se 2class
 
 	model_name = models_directory + 'MLP.hidlay' + str(num_hid_layers) + '.cells' + str(cells_per_layer) + '.drop' + str(dropout_rate)
-	plot_model(model, to_file = model_name + '.png', show_shapes=True)
+	plot_model(model, to_file = model_name + '.2class' +  '.png', show_shapes=True)
 
-	fp_model = open(model_name + '.json', 'w+')
+	fp_model = open(model_name + '.2class' + '.json', 'w+')
 	fp_model.write(model.to_json())
 	fp_model.close()
 	return model
@@ -42,9 +42,9 @@ def fit_and_eval(loss_function, optimizer, dropout_rate, dataset_name):
 	write_results(results_file, 'HiddenLayers|CellsPerLayer|Accuracy')
 	
 	# READ THE INPUT
-	fp_logfile = open('./working/logfile', 'a')
-	reader = Reader(fp_logfile, False)
-	(x_train, y_train), (x_test, y_test) = reader.getDataNormalized()
+	#fp_logfile = open('./working/logfile', 'a')
+	#reader = Reader(fp_logfile, False)
+	(x_train, y_train), (x_test, y_test) = Reader.getDataset(1)
 
 	#(x_train, y_train), (x_test, y_test) = Reader.getDataset(5)
 	#x_train = x_train[0:1000,:]
@@ -52,7 +52,7 @@ def fit_and_eval(loss_function, optimizer, dropout_rate, dataset_name):
 	#x_test = x_test[0:1000,:]
 	#y_test = y_test[0:1000]
 	
-	num_classes = 5
+	num_classes = 2
 	nb_epoch = 20
 
 	lns = []
@@ -61,8 +61,12 @@ def fit_and_eval(loss_function, optimizer, dropout_rate, dataset_name):
 		for cells_per_layer in [20,30,40,50,60]:
 			model = get_model(num_hid_layers, cells_per_layer, dropout_rate)
 			model.compile(optimizer=optimizer, loss=loss_function, metrics=['accuracy'])
-			model.fit(x_train, keras.utils.np_utils.to_categorical(y_train, num_classes), epochs = nb_epoch, batch_size = 128, shuffle=True)
-			ev = model.evaluate(x = x_test, y = keras.utils.np_utils.to_categorical(y_test, num_classes), batch_size = 128)
+			#model.fit(x_train, keras.utils.np_utils.to_categorical(y_train, num_classes), epochs = nb_epoch, batch_size = 128, shuffle=True)
+			model.fit(x_train, y_train, epochs = nb_epoch, batch_size = 128, shuffle=True)
+
+			#ev = model.evaluate(x = x_test, y = keras.utils.np_utils.to_categorical(y_test, num_classes), batch_size = 128)
+			ev = model.evaluate(x = x_test, y = y_test, batch_size = 128)
+
 			results.append(ev[1])
 			#results.append(0.1*num_hid_layers + 0.001*cells_per_layer)		
 			write_results(results_file, str(num_hid_layers) + '|' + str(cells_per_layer) + '|' + str(ev[1]))
@@ -127,3 +131,9 @@ def write_results(results_file, text):
 #fit_and_eval('mse', 'sgd', 0.4, 'Dataset5')							DONE in Z	CORRECT
 #fit_and_eval('categorical_crossentropy', 'rmsprop', 0.4, 'Dataset5')	DONE in Z	with ERROR
 
+
+# Dataset 1 ping attack
+#fit_and_eval('mse', 'sgd', 0.2, 'Dataset1')
+#fit_and_eval('mse', 'sgd', 0.4, 'Dataset1')
+fit_and_eval('binary-crossentropy', 'rmsprop', 0.2, 'Dataset1')
+fit_and_eval('binary-crossentropy', 'rmsprop', 0.4, 'Dataset1')
